b0e8d80bf21c2ce964e5893a365d451598b4dc93
Jangidp0903 <156504199+Jangidp0903@users.noreply.github.com>
2026-05-19 12:02:31 +0530

    add HUB_OPTIONS constant for administrative hub selection

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,8 +1,10 @@
 export const HUB_OPTIONS = [
-  { label: "Lawrence road", value: "Lawrence road" },
-  { label: "Uttam Nagar", value: "Uttam Nagar" },
-  { label: "Peeragarhi", value: "Peeragarhi" },
-  { label: "Lado Sarai", value: "Lado Sarai" },
-  { label: "Okhla", value: "Okhla" },
-  { label: "Noida 73", value: "Noida 73" },
+  { label: "Lawrence road (DEL)", value: "Lawrence road (DEL)" },
+  { label: "Uttam Nagar (DEL)", value: "Uttam Nagar (DEL)" },
+  { label: "Peeragarhi (DEL)", value: "Peeragarhi (DEL)" },
+  { label: "Lado Sarai (DEL)", value: "Lado Sarai (DEL)" },
+  { label: "Okhla (DEL)", value: "Okhla (DEL)" },
+  { label: "Noida 73 (NOIDA)", value: "Noida 73 (NOIDA)" },
+  { label: "Sukhrali (GGN)", value: "Sukhrali (GGN)" },
+  { label: "K.K. Nagar (CHEN)", value: "K.K. Nagar (CHEN)" },
 ];
